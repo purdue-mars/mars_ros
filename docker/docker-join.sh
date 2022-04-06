c79df1bdd526a58bca483f5fd326776f21767495
Harrison McCarty <hmccarty@pm.me>
2022-04-06 17:37:42 -0400

    made a few changes to successfully run sim

---
+++ docker/docker-join.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CONTAINER_NAME="${CONTAINER_NAME:-$USER-mars-dev}"
+
+docker exec -it \
+    -e DISPLAY \
+    -e LIBGL_ALWAYS_INDIRECT \
+    $@ \
+    $CONTAINER_NAME \
+    ${DOCKER_CMD:-/bin/bash}
