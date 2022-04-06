4bdbc33070b23a584690dbfc11b3d6f79958e4da
Harrison McCarty <hmccarty@pm.me>
2022-04-06 12:09:06 -0400

    added docker scripts

---
+++ docker/docker-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DOCKER_DIR=$(realpath $(dirname $0))
+
+docker build --build-arg USER=$USER \
+             --build-arg PW="robot" \
+             --build-arg UID=$(id -u) \
+             --build-arg GID=$(id -g) \
+             --build-arg ROS_DEPS="$ROS_DEPS" \
+             -t mars-icra-2022:local-$USER \
+             -f $DOCKER_DIR/Dockerfile \
+             $@ \
+             $DOCKER_DIR
---
+++ docker/docker-run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+WS_DIR=$(realpath $(dirname $0)/../../../)
+CONTAINER_NAME="${CONTAINER_NAME:-$USER-mars-dev}"
+
+docker run --rm \
+	-it \
+	-e USER \
+	-e DISPLAY \
+	-e NVIDIA_DRIVER_CAPABILITIES=all \
+	-v $XAUTHORITY:/home/$USER/.Xauthority:ro \
+	-v $WS_DIR:/home/$USER/catkin_ws \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	`[ -f ~/.gitconfig ] && echo "-v $HOME/.gitconfig:/home/$USER/.gitconfig:ro"` \
+	-v ~/.ssh:/home/$USER/.ssh:ro \
+	--name $CONTAINER_NAME \
+	$@ \
+	mars-icra-2022:local-$USER \
+	${DOCKER_CMD:+/bin/bash -c "$DOCKER_CMD"}
