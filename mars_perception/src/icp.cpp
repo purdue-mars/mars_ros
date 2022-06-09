522af2b0c7c2eb916e82946a385ddde84716f21f
Raghava <raghava.upp13@gmail.com>
2022-06-09 19:46:31 -0400

    fixed threading issue

--- mars_perception/src/icp.cpp
+++ mars_perception/src/icp.cpp
@@ -114,7 +114,9 @@ bool ICP::mesh_icp_srv(mars_msgs::ICPMeshTF::Request &req, mars_msgs::ICPMeshTF:
     set_mesh_(req.mesh_name);
     mesh_name_ = req.mesh_name;
     std::cout << mesh_name_ << "\n"; 
-    ros::Duration(ICP_CONVERGE_SLEEP_TIME).sleep(); // wait for icp to converge
+    for(int i = 0; i < 10; i++) {
+        run();
+    }
     resp.tf.header.frame_id = mesh_name_ + "_frame";
     resp.tf.header.stamp = ros::Time::now();
     Eigen::Quaternionf q(tf_.topLeftCorner<3, 3>());
